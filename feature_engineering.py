b65e9cdae8c69c108bf65f91685968eabbecd317
chloewang10 <e0420887@u.nus.edu>
2022-10-24 00:15:06 +0800

    nyt_feature_engineer  bug fix

--- feature_engineering.py
+++ feature_engineering.py
@@ -422,10 +422,10 @@ def nyt_feature_engineer(df, df_weights):
 
     nyt_daily = df_out.groupby('date').mean()[['pos_score', 'neg_score', 'neu_score', 'pos_weighted', 'neg_weighted', 'neu_weighted']].sort_index()
 
-    nyt_daily = nyt_daily.set_index('date')
-    nyt_scores = nyt_scores.reindex(pd.date_range(start="2016-01-01", end="2022-09-01"))
-    nyt_scores['date'] = nyt_scores.index
-    nyt_scores = nyt_scores.reset_index(drop = True)
+    nyt_daily = nyt_daily.reindex(pd.date_range(start="2016-01-01", end="2022-09-01"))
+
+    nyt_daily = nyt_daily.reset_index()
+    nyt_daily = nyt_daily.rename(columns={'index':'date'})
 
     # impute NA values using mean of last 3 days
     null_idx = nyt_daily[nyt_daily.isnull().any(axis=1)].index
@@ -433,7 +433,10 @@ def nyt_feature_engineer(df, df_weights):
     for i in null_idx:
         for col in score_cols:
             nyt_daily.at[i,col] = (nyt_daily.at[i-1,col]+nyt_daily.at[i-2,col]+nyt_daily.at[i-3,col])/3
-            
+
+    nyt_daily = nyt_daily.set_index(nyt_daily['date'])
+    nyt_daily = nyt_daily.iloc[: , 1:]   
+
     return nyt_daily
         
 
